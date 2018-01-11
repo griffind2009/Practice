4144ef8d99927f09bc52d8c0d95f40ef2eb83ad3
Griffin <domonique.griffin@accenturefederal.com>
2018-01-11 13:31:08 -0500

    working on sass

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 var compliment = [
   "You can do it",
   "You are almost there",
-  "I believe in you"
+  "I believe in you",
+  "Way to go!"
 ]
 
-var color = ["#F21313", "#3B13EF", "#12ED80", "#F9FF60"]
+var color = ["#F21313", "#3B13EF", "#12ED80", "#F9FF60", "#191970"]
 
-var compliments = compliment[(Math.floor(Math.random()*3))]
-var colors = color[(Math.floor(Math.random()*4))]
+var compliments = compliment[(Math.floor(Math.random()*4))]
+var colors = color[(Math.floor(Math.random()*5))]
 
 function upDate(background) {
   document.getElementById('background').innerHTML = compliments
